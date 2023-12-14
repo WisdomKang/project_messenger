657271937e23d19bafe8354d3313fe9a449c9fa6
WisdomKang <buckgusaud@gmail.com>
2023-12-14 21:39:28 +0900

    setting firebase_options.dart delete

--- application/lib/firebase/firebase_message_setting.dart
+++ application/lib/firebase/firebase_message_setting.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:my_messenger_application/firebase_options.dart';
+import 'package:my_messenger_application/firebase/firebase_options.dart';
 
 Future<void> initializeFirebaseMessaging() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
