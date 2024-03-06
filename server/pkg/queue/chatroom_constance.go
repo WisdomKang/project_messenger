8a3a0e53490103ec99dc17bfb240b76caa0392d9
WisdomKang <buckgusaud@gmail.com>
2024-03-06 16:21:43 +0900

    create : queue setting

---
+++ server/pkg/queue/chatroom_constance.go
@@ -0,0 +1,13 @@
+package queue
+
+const (
+	SETTING_FILE_NAME = "\\server_config.yaml"
+
+	MAIN_EXCHANGE = "MAIN_EXCHANGE"
+
+	QUEUE_TPYE_CHAT = "chatroom"
+	QUEUE_TPYE_USER = "user"
+
+	PREFIX_CHAT_QUEUE = "CHATROOM"
+	PREFIX_USER_QUEUE = "USERS"
+)
