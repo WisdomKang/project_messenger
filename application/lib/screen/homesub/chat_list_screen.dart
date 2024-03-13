959f7ecccbce8d2f5187a1cd81109f452ee43ba6
WisdomKang <buckgusaud@gmail.com>
2024-03-13 12:13:25 +0900

    save : #1

--- application/lib/screen/homesub/chat_list_screen.dart
+++ application/lib/screen/homesub/chat_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_messenger_application/screen/screen_name.dart';
 
 class ChatListScreen extends StatefulWidget {
   const ChatListScreen({super.key});
@@ -15,20 +16,35 @@ class _ChatListScreenState extends State<ChatListScreen> {
       child: ListView(
         padding: EdgeInsets.all(8.0),
         children: [
-          Row(
-            children: [
-              Text("Notification"),
-              Switch(
-                value: true,
-                activeColor: Colors.lime,
-                onChanged: (value) {
+          ChatRoomInfo(roomId: "1", roomName: "친구"),
+          ChatRoomInfo(roomId: "2", roomName: "마음"),
 
-                },
-              )
-            ],
-          ),
+          ChatRoomInfo(roomId: "3", roomName: "용기"),
+          ChatRoomInfo(roomId: "4", roomName: "믿음"),
+          ChatRoomInfo(roomId: "5", roomName: "사랑"),
+          
         ],
       )
+    );
+  }
+}
+
+class ChatRoomInfo extends StatelessWidget {
+  String roomId;
+  String roomName;
+
+
+  ChatRoomInfo({super.key, required this.roomId, required this.roomName});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(roomName),
+      leading: CircleAvatar(),
+      onTap: (){
+        print( "$roomId is tapped!");
+        Navigator.of(context).pushNamed(CHATTING_SCREEN);
+      },
     );
   }
 }
